c3c0627c217cbb5fd5a16b5579a104d85a2bc606
Miguel <miguelgao@hotmail.com>
2018-05-07 10:08:39 -0500

    layout creacion servicio

---
+++ app/src/main/java/finalproject/android/uniquindio/co/proyectoandroid/C_servicio.kt
@@ -0,0 +1,12 @@
+package finalproject.android.uniquindio.co.proyectoandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class C_servicio : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c_servicio)
+    }
+}
